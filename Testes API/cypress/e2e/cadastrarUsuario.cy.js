9b16552fcb529d13669e428bf4fca264d0a6754a
ItaloRVieira <italorenan290898@gmail.com>
2024-05-31 12:07:39 -0300

    teste criar usuario nome com 100 caracteres api

--- Testes API/cypress/e2e/cadastrarUsuario.cy.js
+++ Testes API/cypress/e2e/cadastrarUsuario.cy.js
@@ -40,7 +40,7 @@ describe('Testes criação de usuário', () => {
         })
     });
 
-    it('Criar conta informando e-mail com 60 dígitos deve retornar 201', () => {
+    it('Criar conta informando e-mail com 60 caracteres deve retornar 201', () => {
         let name = faker.person.firstName()
         let emailValido = faker.random.alpha({ count: 48 }).toLowerCase() + '@dominio.net'
         cy.request({
@@ -62,7 +62,7 @@ describe('Testes criação de usuário', () => {
         })
     })
 
-    it('Criar conta informando nome com 1 dígitos deve retornar 201', () => {
+    it('Criar conta informando nome com 1 caractere deve retornar 201', () => {
         let name = '%'
         let emailValido = faker.random.alpha({count: 7}).toLowerCase() + '@dominio.com'
         cy.request({
@@ -84,7 +84,29 @@ describe('Testes criação de usuário', () => {
         })
     })
 
-    it('Criar conta informando e-mail com 5 dígitos deve retornar 201', () => {
+    it('Criar conta informando nome com 100 caracteres deve retornar 201', () => {
+        let name = faker.random.alpha({count: 100})
+        let emailValido = faker.random.alpha({count: 7}).toLowerCase() + '@dominio.com'
+        cy.request({
+            method: 'POST',
+            url: '/api/users',
+            body: {
+                "name": name,
+                "email": emailValido,
+                "password": password
+            }
+        }).then((response) => {
+            expect(response.status).to.eq(201);
+            expect(response.body.name).to.eq(name);
+            expect(response.body.email).to.eq(emailValido);
+            expect(response.body.type).to.eq(0)
+            expect(response.body.active).to.eq(true)
+            userid = response.body.id;
+            cy.wrap(emailValido).as("email")
+        })
+    })
+
+    it('Criar conta informando e-mail com 5 caracteres deve retornar 201', () => {
         let name = faker.person.firstName()
         let emailValido = 'u@g.p'
         cy.request({
