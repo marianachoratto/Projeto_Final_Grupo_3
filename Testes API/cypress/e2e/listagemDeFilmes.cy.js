b26a2f7c528eaf1ee2033f2f14e6d8ccce769887
marianachoratto <marianachoratto@gmail.com>
2024-06-04 23:55:10 -0300

    revisando testes de gabriel

--- Testes API/cypress/e2e/listagemDeFilmes.cy.js
+++ Testes API/cypress/e2e/listagemDeFilmes.cy.js
@@ -1,129 +1,148 @@
-
-describe('Qualquer tipo de usuario pode ver a lista de filmes', () => {
-
-  it('É possivel ver a lista de filmes estando sem logar', () => {
-    cy.request({
-      method: "GET",
-      url: "api/movies",
-
-    }).then((resposta) => {
-      expect(resposta.status).to.eq(200);
-      resposta.body.forEach(notaDosFilmes => {
-        expect(notaDosFilmes).to.have.property('totalRating');
-        expect(notaDosFilmes).to.have.property('id');
-        expect(notaDosFilmes).to.have.property('genre');
-        expect(notaDosFilmes).to.have.property('description');
-        expect(notaDosFilmes).to.have.property('durationInMinutes');
-        expect(notaDosFilmes).to.have.property('releaseYear');
-      })
-    })
-  });
-
-});
-
-describe('Qualquer tipo de usuario pode ver a lista de filmes', () => {
-  let dadosUser
-  before(() => {
-    cy.cadastrarUsuario('123456').then((response) => {
-      dadosUser = response
-    })
-  });
-  
-  after(() => {
-    cy.deletarUsuario(dadosUser.email, '123456', dadosUser.id);
-  });
-
-  it('É possivel ver a lista de filmes estando logado como usuario comum', () => {
-    cy.loginValido(dadosUser.email, '123456')
-    cy.request({
-      method: "GET",
-      url: "api/movies",
-    }).then((resposta) => {
-      expect(resposta.status).to.eq(200);
-      resposta.body.forEach(notaDosFilmes => {
-        expect(notaDosFilmes).to.have.property('totalRating');
-        expect(notaDosFilmes).to.have.property('id');
-        expect(notaDosFilmes).to.have.property('genre');
-        expect(notaDosFilmes).to.have.property('description');
-        expect(notaDosFilmes).to.have.property('durationInMinutes');
-        expect(notaDosFilmes).to.have.property('releaseYear');
-      })
-    })
-  });
-
-});
-
-describe('Qualquer tipo de usuario pode ver a lista de filmes', () => {
-  let dadosUser
-  before(() => {
-        cy.cadastrarUsuario('123456').then((response) => {
-          dadosUser = response
-        })
+describe("Listagem de filmes", () => {
+  let token;
+  let dadosUser;
+  before(function () {
+    cy.cadastrarUsuario().then((resposta) => {
+      dadosUser = resposta;
+      cy.loginValido(dadosUser.email, dadosUser.password).then(function (
+        resposta
+      ) {
+        token = resposta.token;
+        cy.promoverAdmin(token).then(function (resposta) {
+          cy.criarFilme(token);
+        });
       });
-  before(() => {
-    cy.LogarPromoverCritico(dadosUser.email,'123456',).then(() => {
-    })
-  });
-  
-  after(() => {
-    cy.deletarUsuario(dadosUser.email, '123456', dadosUser.id);
+    });
   });
 
-  it('É possivel ver a lista de filmes estando logado como usuario critico', () => {
-    cy.loginValido(dadosUser.email, '123456')
+  it("É possivel usário ver a lista de filmes estando sem logar", () => {
     cy.request({
       method: "GET",
       url: "api/movies",
     }).then((resposta) => {
+      // ver quantos filmes a lista retorna
       expect(resposta.status).to.eq(200);
-      resposta.body.forEach(notaDosFilmes => {
-        expect(notaDosFilmes).to.have.property('totalRating');
-        expect(notaDosFilmes).to.have.property('id');
-        expect(notaDosFilmes).to.have.property('genre');
-        expect(notaDosFilmes).to.have.property('description');
-        expect(notaDosFilmes).to.have.property('durationInMinutes');
-        expect(notaDosFilmes).to.have.property('releaseYear');
-      })
-    })
+      expect(resposta.body[0]).to.have.property("totalRating");
+      expect(resposta.body[0]).to.have.property("id");
+      expect(resposta.body[0]).to.have.property("genre");
+      expect(resposta.body[0]).to.have.property("description");
+      expect(resposta.body[0]).to.have.property("durationInMinutes");
+      expect(resposta.body[0]).to.have.property("releaseYear");
+    });
   });
 
-});
+  it("Deve ser possível fazer a listagem de todos filmes", function () {
+    // Pesquisar sem nome do filme
+    // VER QUANTOS FILMES A LISTA RETORNA
+    // Exemplos de asserções:
+    // arrayNumber = resposta.body.length;
+    // expect(resposta.status).to.equal(200);
+    // expect(resposta.body).to.be.an("array");
+    // expect(resposta.body.length).to.deep.equal(arrayNumber);
+  });
 
-describe('Qualquer tipo de usuario pode ver a lista de filmes', () => {
-  let dadosUser
-  before(() => {
-        cy.cadastrarUsuario('123456').then((response) => {
-          dadosUser = response
-        })
+  it("Deve ser possível a consulta de filmes pelo título", function () {});
+
+  describe("Listagem de filme de usuário logado", function () {
+    let dadosUser;
+    let token;
+
+    before(function () {
+      cy.cadastrarUsuario().then((resposta) => {
+        dadosUser = resposta;
+        cy.loginValido(dadosUser.email, dadosUser.password).then(function (
+          resposta
+        ) {
+          token = resposta.token;
+          cy.promoverAdmin(token).then(function (resposta) {
+            cy.criarFilme(token);
+          });
+        });
       });
-  before(() => {
-    cy.LogarPromoverADM(dadosUser.email,'123456',).then(() => {
-    })
-  });
-  
-  after(() => {
-    cy.deletarUsuario(dadosUser.email, '123456', dadosUser.id);
+    });
+
+    beforeEach(() => {
+      cy.cadastrarUsuario().then((response) => {
+        dadosUser = response;
+      });
+    });
+
+    afterEach(function () {
+      cy.loginValido(dadosUser.email, dadosUser.password).then(function (
+        resposta
+      ) {
+        token = resposta.token;
+        cy.promoverAdmin(token).then(function (resposta) {
+          cy.excluirUsuario(dadosUser.id, token);
+          cy.deletarFilme();
+        });
+      });
+    });
+
+    it("É possivel ver a lista de filmes estando logado como usuario comum", () => {
+      cy.loginValido(dadosUser.email, dadosUser.password);
+      cy.request({
+        method: "GET",
+        url: "api/movies",
+      }).then((resposta) => {
+        // ver quantos filmes a lista retorna
+        expect(resposta.status).to.eq(200);
+        expect(resposta.body[0]).to.have.property("totalRating");
+        expect(resposta.body[0]).to.have.property("id");
+        expect(resposta.body[0]).to.have.property("genre");
+        expect(resposta.body[0]).to.have.property("description");
+        expect(resposta.body[0]).to.have.property("durationInMinutes");
+        expect(resposta.body[0]).to.have.property("releaseYear");
+      });
+    });
+
+    it("É possivel ver a lista de filmes estando logado como usuario critico", () => {
+      cy.loginValido(dadosUser.email, dadosUser.password).then(function (
+        resposta
+      ) {
+        token = resposta.token;
+        cy.promoverCritico(token).then(function (resposta) {
+          cy.request({
+            method: "GET",
+            url: "api/movies",
+          }).then((resposta) => {
+            // ver quantos filmes a lista retorna
+            expect(resposta.status).to.eq(200);
+            expect(resposta.body[0]).to.have.property("totalRating");
+            expect(resposta.body[0]).to.have.property("id");
+            expect(resposta.body[0]).to.have.property("genre");
+            expect(resposta.body[0]).to.have.property("description");
+            expect(resposta.body[0]).to.have.property("durationInMinutes");
+            expect(resposta.body[0]).to.have.property("releaseYear");
+          });
+        });
+      });
+    });
+
+    it("É possivel ver a lista de filmes estando logado como usuario ADM", () => {
+      let token;
+      cy.loginValido(dadosUser.email, dadosUser.password).then((resposta) => {
+        token = resposta.token;
+
+        cy.promoverAdmin(token);
+        cy.request({
+          method: "GET",
+          url: "api/movies",
+        }).then((resposta) => {
+          expect(resposta.status).to.eq(200);
+          // ver quantos filmes a lista retorna
+          expect(resposta.body[0]).to.have.property("totalRating");
+          expect(resposta.body[0]).to.have.property("id");
+          expect(resposta.body[0]).to.have.property("genre");
+          expect(resposta.body[0]).to.have.property("description");
+          expect(resposta.body[0]).to.have.property("durationInMinutes");
+          expect(resposta.body[0]).to.have.property("releaseYear");
+        });
+      });
+    });
   });
 
-  it('É possivel ver a lista de filmes estando logado como usuario ADM', () => {
-    cy.loginValido(dadosUser.email, '123456')
-    cy.request({
-      method: "GET",
-      url: "api/movies",
-    }).then((resposta) => {
-      expect(resposta.status).to.eq(200);
-      resposta.body.forEach(notaDosFilmes => {
-        expect(notaDosFilmes).to.have.property('totalRating');
-        expect(notaDosFilmes).to.have.property('id');
-        expect(notaDosFilmes).to.have.property('genre');
-        expect(notaDosFilmes).to.have.property('description');
-        expect(notaDosFilmes).to.have.property('durationInMinutes');
-        expect(notaDosFilmes).to.have.property('releaseYear');
-      })
-    })
+  it("Consulta de filme com filme inexistente não deve retornar nenhum filme", function () {
+    // checar lista vazia
   });
-
 });
-
-
-
--- Testes API/cypress/support/commands.js
+++ Testes API/cypress/support/commands.js
@@ -1,103 +1,63 @@
 import { faker } from "@faker-js/faker";
 const apiUrl = "https://raromdb-3c39614e42d4.herokuapp.com/";
-let dadosUser
-let token
+let email;
+let password = faker.internet.password(6);
+let idNovoUsuario;
+let nome;
+let tokenid;
 
-Cypress.Commands.add("deletarUsuario", (email, password, idNovoUsuario) => {
+// Commands de Usuários
+Cypress.Commands.add("cadastrarUsuario", () => {
   return cy
     .request({
       method: "POST",
-      url: apiUrl + "api/auth/login",
+      url: apiUrl + "api/users",
       body: {
+        name: "faker " + faker.person.firstName(),
+        email: faker.internet.email(),
+        password: password,
+      },
+    })
+    .then(function (resposta) {
+      idNovoUsuario = resposta.body.id;
+
+      nome = resposta.body.name;
+      email = resposta.body.email;
+
+      return cy.wrap({
+        nome: nome,
         email: email,
+        id: idNovoUsuario,
         password: password,
-      },
-    })
-    .then(function (resposta) {
-      token = resposta.body.accessToken;
-
-      cy.request({
-        method: "PATCH",
-        url: apiUrl + "api/users/admin",
-        auth: {
-          bearer: token,
-        },
-      });
-    })
-    .then(function () {
-      cy.request({
-        method: "DELETE",
-        url: apiUrl + `api/users/${idNovoUsuario}`,
-        auth: {
-          bearer: token,
-        },
       });
     });
 });
 
-Cypress.Commands.add("cadastrarUsuario", (password) => {
-  return cy
-    .request({
-      method: "POST",
-      url: apiUrl + "api/users",
-      body: {
-        name: faker.person.firstName(),
-        email: faker.internet.email(),
-        password: password,
-      },
-    })
-    .then(function (resposta) {
-      dadosUser = resposta.body
-      return cy.wrap(dadosUser);
-    });
+Cypress.Commands.add("loginValido", (email, password) => {
+  cy.request({
+    method: "POST",
+    url: "/api/auth/login",
+    body: {
+      email: email,
+      password: password,
+    },
+  }).then(function (resposta) {
+    tokenid = resposta.body.accessToken;
+
+    return {
+      token: tokenid,
+    };
+  });
 });
 
-Cypress.Commands.add("CadastrarEPromoverAdmin", (password) => {
-  return cy
-    .request({
-      method: "POST",
-      url: apiUrl + "api/users",
-      body: {
-        name: "faker " + faker.person.firstName(),
-        email: faker.internet.email(),
-        password: password,
-      },
-    })
-    .then(function (resposta) {
-      token = resposta.body.accessToken;
-
-      cy.request({
-        method: "PATCH",
-        url: apiUrl + "api/users/admin",
-        auth: {
-          bearer: token,
-        },
-      });
-    });
-});
-
-Cypress.Commands.add("CadastrarEPromoverAdmin", (password) => {
-  return cy
-    .request({
-      method: "POST",
-      url: apiUrl + "api/users",
-      body: {
-        name: "faker " + faker.person.firstName(),
-        email: faker.internet.email(),
-        password: password,
-      },
-    })
-    .then(function (resposta) {
-      token = resposta.body.accessToken;
-
-      cy.request({
-        method: "PATCH",
-        url: apiUrl + "api/users/admin",
-        auth: {
-          bearer: token,
-        },
-      });
-    });
+Cypress.Commands.add("promoverAdmin", (tokenid) => {
+  cy.request({
+    method: "PATCH",
+    url: "/api/users/admin",
+    headers: {
+      Authorization: `Bearer ${tokenid}`,
+    },
+  });
 });
 
 Cypress.Commands.add("promoverCritico", function (tokenid) {
@@ -110,28 +70,7 @@ Cypress.Commands.add("promoverCritico", function (tokenid) {
   });
 });
 
-Cypress.Commands.add("promoverAdmin", (tokenid) => {
-  cy.request({
-    method: "PATCH",
-    url: apiUrl + "api/users/admin",
-    headers: {
-      Authorization: `Bearer ${tokenid}`,
-    },
-  });
-});
-
-Cypress.Commands.add("loginValido", function (email, password) {
-  cy.request({
-    method: "POST",
-    url: apiUrl + "api/auth/login",
-    body: {
-      email: email,
-      password: password,
-    },
-  });
-});
-
-Cypress.Commands.add("excluirUsuario", function (userid, tokenid) {
+Cypress.Commands.add("excluirUsuario", (userid, tokenid) => {
   cy.request({
     method: "DELETE",
     url: apiUrl + "api/users/" + userid,
@@ -141,48 +80,58 @@ Cypress.Commands.add("excluirUsuario", function (userid, tokenid) {
   });
 });
 
-Cypress.Commands.add("LogarPromoverCritico", (email, password,) => {
+// Commands de filme
+Cypress.Commands.add("deletarFilme", (movieId, tokenid) => {
+  cy.request({
+    method: "DELETE",
+    url: apiUrl + "api/movies/" + movieId,
+    headers: {
+      Authorization: `Bearer ${tokenid}`,
+    },
+  });
+  // cy.promoverAdmin(tokenid).then(function (resposta) {
+  // });
+});
+
+Cypress.Commands.add("criarUsuario", (name, emailValido, password) => {
+  cy.request({
+    method: "POST",
+    url: "/api/users",
+    body: {
+      name: name,
+      email: emailValido,
+      password: password,
+    },
+  });
+});
+
+// Commands de filme
+Cypress.Commands.add("deletarFilme", (email, password) => {});
+
+Cypress.Commands.add("criarFilme", (userToken) => {
   return cy
     .request({
       method: "POST",
-      url: apiUrl + "api/auth/login",
+      url: "/api/movies",
+      auth: {
+        bearer: userToken,
+      },
       body: {
-        email: email,
-        password: password,
+        title: faker.internet.userName(),
+        genre: faker.internet.password(8),
+        description: faker.internet.email(),
+        durationInMinutes: 100,
+        releaseYear: 2022,
       },
     })
-    .then(function (resposta) {
-      token = resposta.body.accessToken;
-
-      cy.request({
-        method: "PATCH",
-        url: apiUrl + "api/users/apply",
-        auth: {
-          bearer: token,
-        },
-      });
-    })
-  })
-
-  Cypress.Commands.add("LogarPromoverADM", (email, password,) => {
-    return cy
-      .request({
-        method: "POST",
-        url: apiUrl + "api/auth/login",
-        body: {
-          email: email,
-          password: password,
-        },
-      })
-      .then(function (resposta) {
-        token = resposta.body.accessToken;
-  
-        cy.request({
-          method: "PATCH",
-          url: apiUrl + "api/users/admin",
-          auth: {
-            bearer: token,
-          },
-        });
-      })
-    })
+    .then((resposta) => {
+      return {
+        id: resposta.body.id,
+        title: resposta.body.title,
+        genre: resposta.body.genre,
+        description: resposta.body.description,
+        durationInMinutes: 100,
+        releaseYear: 2022,
+      };
+    });
+});
