8f4326796c91117eee7aba78e39bae5c2a1580ce
ItaloRVieira <italorenan290898@gmail.com>
2024-06-06 12:26:25 -0300

    promover crítico para admin

--- Testes API/cypress/e2e/promoverUsuario.cy.js
+++ Testes API/cypress/e2e/promoverUsuario.cy.js
@@ -71,4 +71,31 @@ describe('Promoção de usuário', () => {
             expect(response.body.active).to.eq(true)
         })
     }); 
+
+    it('É possível promover usuário crítico para usuário admin', () => {
+        cy.promoverCritico(token);
+        cy.request({
+            method: "PATCH",
+            url: "api/users/admin",
+            headers: {
+                Authorization: `Bearer ${token} `,
+            },
+        }).then((response) => {
+            expect(response.status).to.eq(204)
+        });
+
+        cy.request({
+            method: "GET",
+            url: "api/users/" + id,
+            headers: {
+                Authorization: `Bearer ${token} `,
+            },
+        }).then((response) => {
+            expect(response.body.id).to.eq(id)
+            expect(response.body.name).to.eq(name)
+            expect(response.body.email).to.eq(email)
+            expect(response.body.type).to.eq(1)
+            expect(response.body.active).to.eq(true)
+        })
+    }); 
 })
