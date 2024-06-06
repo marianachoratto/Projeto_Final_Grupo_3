37a0c8b3eee179ec7e908d6e225c8c1d9b14b487
ItaloRVieira <italorenan290898@gmail.com>
2024-06-06 12:10:04 -0300

    Cenários promover usuário

---
+++ Testes API/cypress/e2e/promoverUsuario.cy.js
@@ -0,0 +1,48 @@
+describe('Promoção de usuário', () => {
+    let id;
+    let token;
+    let name
+    let email;
+    let password;
+    beforeEach(() => {
+        cy.cadastrarUsuario().then((resposta) => {
+            id = resposta.id;
+            name = resposta.name;
+            email = resposta.email;
+            password = resposta.password;
+            cy.loginValido(email, password).then((resposta) => {
+                token = resposta.body.accessToken;
+            })
+        })
+    })
+    afterEach(() => {
+        cy.promoverAdmin(token);
+        cy.excluirUsuario(id, token);
+    });
+
+    it('É possível promover usuário comum para usuário crítico', () => {
+        cy.request({
+            method: "PATCH",
+            url: "api/users/apply",
+            headers: {
+                Authorization: `Bearer ${token} `,
+            },
+        }).then((response) => {
+            expect(response.status).to.eq(204)
+        });
+
+        cy.request({
+            method: "GET",
+            url: "api/users/" + id,
+            headers: {
+                Authorization: `Bearer ${token} `,
+            },
+        }).then((response) => {
+            expect(response.body.id).to.eq(id)
+            expect(response.body.nome).to.eq(name)
+            expect(response.body.email).to.eq(email)
+            expect(response.body.type).to.eq(2)
+            expect(response.body.active).to.eq(true)
+        })
+    });
+})
