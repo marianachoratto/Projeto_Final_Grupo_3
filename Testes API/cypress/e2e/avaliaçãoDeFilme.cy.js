43bd17139b21092146b7ef82b8ea1f3387a090de
marianachoratto <marianachoratto@gmail.com>
2024-06-07 12:00:26 -0300

    melhorando testes de criação de filmes

---
+++ Testes API/cypress/e2e/avaliaçãoDeFilme.cy.js
@@ -0,0 +1,11 @@
+describe("Testes de Avaliação de Filme", function () {
+  it("Usuário sem autenticação não deve conseguir fazer review de filme", function () {});
+
+  describe("Avaliação de filmes com sucesso", function () {
+    it("O usuário logado deve conseguir fazer uma avaliação de filme", function () {});
+
+    it("Checando se a avaliação foi feita", function () {});
+
+    it("Deve ser possível fazer avaliação de filme sem texto", function () {});
+  });
+});
