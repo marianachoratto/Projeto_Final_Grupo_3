6a88c2e6c723f69cfc0458b4de6dafae97d8f273
ItaloRVieira <italorenan290898@gmail.com>
2024-06-10 14:27:36 -0300

    Acrescentando novos cenarios consulta de filmes

--- Testes API/cypress/e2e/consultaDeFilmes.cy.js
+++ Testes API/cypress/e2e/consultaDeFilmes.cy.js
@@ -77,7 +77,6 @@ describe('Consultado informações detalhadas de filmes', () => {
             expect(response.body.criticScore).to.eq(0)
             expect(response.body.durationInMinutes).to.eq(movieDuration)
             expect(response.body.releaseYear).to.eq(movieYear)
-            expect(response.body.reviews).to.be.a("array")
         });
     })
     it('É possível consultar detalhes de um filme com usuário comum', () => {
@@ -127,7 +126,6 @@ describe('Consultado informações detalhadas de filmes', () => {
             expect(response.body.criticScore).to.eq(0)
             expect(response.body.durationInMinutes).to.eq(movieDuration)
             expect(response.body.releaseYear).to.eq(movieYear)
-            expect(response.body.reviews).to.be.a("array")
         });
     })
     it('É possível consultar detalhes de um filme com usuário crítico', () => {
@@ -178,7 +176,92 @@ describe('Consultado informações detalhadas de filmes', () => {
             expect(response.body.criticScore).to.eq(0)
             expect(response.body.durationInMinutes).to.eq(movieDuration)
             expect(response.body.releaseYear).to.eq(movieYear)
+        });
+    })
+    it('É possível consultar detalhes de um filme com usuário admin', () => {
+        cy.cadastrarUsuario().then((resposta) => {
+            userid1 = resposta.id;
+            name1 = resposta.nome;
+            email1 = resposta.email;
+            password = resposta.password
+            cy.loginValido(email1, password).then((resposta) => {
+                token1 = resposta.body.accessToken;
+                cy.reviewMovie1(token1, movieId)
+            })
+        })
+        cy.cadastrarUsuario().then((resposta) => {
+            userid2 = resposta.id;
+            name2 = resposta.nome;
+            email2 = resposta.email;
+            password = resposta.password
+            cy.loginValido(email2, password).then((resposta) => {
+                token2 = resposta.body.accessToken;
+                cy.reviewMovie2(token2, movieId)
+            })
+        })
+
+        cy.cadastrarUsuario().then((resposta) => {
+            userid = resposta.id
+            email = resposta.email;
+            password = resposta.password;
+            cy.loginValido(email, password).then((resposta) => {
+                token = resposta.body.accessToken
+                cy.promoverAdmin(token)
+            })
+        })
+        
+        cy.request({
+            method: "GET",
+            url: 'api/movies/' + movieId,
+            headers: {
+                Authorization: "Bearer " + token,
+              }
+        }).then((response) => {
+            expect(response.status).to.eq(200)
+            expect(response.body.title).to.eq(movieTitle)
+            expect(response.body.id).to.eq(movieId)
+            expect(response.body.description).to.eq(movieDescription)
+            expect(response.body.genre).to.eq(movieGenre)
+            expect(response.body.audienceScore).to.eq(3)
+            expect(response.body.criticScore).to.eq(0)
+            expect(response.body.durationInMinutes).to.eq(movieDuration)
+            expect(response.body.releaseYear).to.eq(movieYear)
+        });
+    })
+    it('Retorna todas as reviews de um filme ao realizar consulta', () => {
+        cy.cadastrarUsuario().then((resposta) => {
+            userid1 = resposta.id;
+            name1 = resposta.nome;
+            email1 = resposta.email;
+            password = resposta.password
+            cy.loginValido(email1, password).then((resposta) => {
+                token1 = resposta.body.accessToken;
+                cy.reviewMovie1(token1, movieId)
+            })
+        })
+        cy.cadastrarUsuario().then((resposta) => {
+            userid2 = resposta.id;
+            name2 = resposta.nome;
+            email2 = resposta.email;
+            password = resposta.password
+            cy.loginValido(email2, password).then((resposta) => {
+                token2 = resposta.body.accessToken;
+                cy.reviewMovie2(token2, movieId)
+            })
+        })
+
+        cy.cadastrarUsuario().then((resposta) => {
+            userid = resposta.id
+            email = resposta.email;
+            password = resposta.password;
+        })
+        cy.request({
+            method: "GET",
+            url: 'api/movies/' + movieId
+        }).then((response) => {
+            expect(response.status).to.eq(200)
             expect(response.body.reviews).to.be.a("array")
+            expect(response.body.reviews.length).to.eq(2)
         });
     })
 })
