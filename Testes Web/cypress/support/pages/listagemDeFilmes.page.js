f9a7a5fcc41f83e986943cba6e6fbcb425ac9358
pelluzzio <gabrielpelluzio@gmail.com>
2024-06-11 15:39:40 -0300

    revisao

--- Testes Web/cypress/support/pages/listagemDeFilmes.page.js
+++ Testes Web/cypress/support/pages/listagemDeFilmes.page.js
@@ -14,74 +14,97 @@ export default class listagemDeFilmes {
     headerFilmesEmDestaque = '.featured-movies > .section-header > h3';
     headerFilmesBemAvaliados = '.top-rated-movies > .section-header > h3';
     msgErro = 'p';
-
+    cardFilme = '[href="/movies/23"] > .movie-card-footer';
+    gridFilme = '.movie-details-container';
+    nota1 = '.top-rated-movies > .carousel-container > .carousel-data > [href="/movies/23"] > .movie-card-footer > .bad';
+    nota2 = '.top-rated-movies > .carousel-container > .carousel-data > [href="/movies/27"] > .movie-card-footer > .bad';
+    nota3 = '.top-rated-movies > .carousel-container > .carousel-data > [href="/movies/28"] > .movie-card-footer > .good';
+    nota4 = '.top-rated-movies > .carousel-container > .carousel-data > [href="/movies/29"] > .movie-card-footer > .good';
     getLogo (){
-        return cy.get(this.linkLogin).should('be.visible').click({ multiple: true });
+        return cy.get(this.linkLogin).click({ multiple: true });
     };
     getPesquisa (){
-        return cy.get(this.formPesquisa).should('be.visible').type('Teste');
+        return cy.get(this.formPesquisa).type('Teste');
     };
     getEnviar (){
-        return cy.get(this.buttonEnviar).should('be.visible').click();
+        return cy.get(this.buttonEnviar).click();
     };
     getFilmes (){
-        return cy.get(this.linkFilmes).should('be.visible').click({ multiple: true });
+        return cy.get(this.linkFilmes).click({ multiple: true });
     };
     getLogin (){
-        return cy.get(this.linkLogin).should('be.visible').click();
+        return cy.get(this.linkLogin).click();
     };
     getRegistro (){
-        return cy.get(this.linkRegistro).should('be.visible').click();
+        return cy.get(this.linkRegistro).click();
     };
     getDestaque (){
-        return cy.get(this.carouselDestaque).should('be.visible');
+        return cy.get(this.carouselDestaque);
     };
     getBemAvaliados (){
-        return cy.get(this.carouselBemAvaliados).should('be.visible');
+        return cy.get(this.carouselBemAvaliados);
     };
     getBAesquerda (){
-        return cy.get(this.carouselBAsetaEsquerda).should('be.visible');
+        return cy.get(this.carouselBAsetaEsquerda);
     };
     getBAdireita (){
-        return cy.get(this.carouselBAsetaDireita).should('be.visible')
+        return cy.get(this.carouselBAsetaDireita)
     };
     getFilmesBemAvaliados (){
-        return  cy.get(this.headerFilmesBemAvaliados).should('be.visible')
+        return  cy.get(this.headerFilmesBemAvaliados)
     };
     getFilmesEmDestaque (){
-        return  cy.get(this.headerFilmesEmDestaque).should('be.visible')
+        return  cy.get(this.headerFilmesEmDestaque)
     };
     getDesquerda (){
-        return cy.get(this.carouselDEsquerda).should('be.visible')
+        return cy.get(this.carouselDEsquerda)
     };
     getDdireita (){
-        return cy.get(this.carouselDDireita).should('be.visible')
+        return cy.get(this.carouselDDireita)
     };
     getDesquerda1 (){
-        return cy.get(this.carouselDEsquerda).should('be.visible').should('be.enabled').click();
+        return cy.get(this.carouselDEsquerda).click();
     };
     getDdireita1 (){
-        return cy.get(this.carouselDDireita).should('be.visible').should('be.enabled').click();
+        return cy.get(this.carouselDDireita).click();
     };
     getDesquerda2 (){
-        return cy.get(this.carouselDEsquerda).should('be.visible').should('not.be.enabled');
+        return cy.get(this.carouselDEsquerda);
     };
     getDdireita2 (){
-        return cy.get(this.carouselDDireita).should('be.visible').should('not.be.enabled');
+        return cy.get(this.carouselDDireita);
     };
     getBAesquerda1 (){
-        return cy.get(this.carouselBAsetaEsquerda).should('be.visible').should('be.enabled').click();
+        return cy.get(this.carouselBAsetaEsquerda).click();
     };
     getBAdireita1 (){
-        return cy.get(this.carouselBAsetaDireita).should('be.visible').should('be.enabled').click();
+        return cy.get(this.carouselBAsetaDireita).click();
     };
     getBAesquerda2 (){
-        return cy.get(this.carouselBAsetaEsquerda).should('be.visible').should('be.visible').should('not.be.enabled');
+        return cy.get(this.carouselBAsetaEsquerda);
     };
     getBAdireita2 (){
-        return cy.get(this.carouselBAsetaDireita).should('be.visible').should('be.visible').should('not.be.enabled');
+        return cy.get(this.carouselBAsetaDireita);
     };
     getMsgerro (){
-        return cy.get(this.msgErro).should('be.visible').should('have.text','Ops! Parece que ainda não temos nenhum filme.')
-    }
+        return cy.get(this.msgErro)
+    };
+    getCardFilme (){
+        return  cy.get(this.cardFilme).eq(0).click()
+    };
+    getDetalhes (){
+        return  cy.get(this.gridFilme)
+    };
+    getNota1 (){
+        return  cy.get(this.nota1)
+    };
+    getNota2 (){
+        return  cy.get(this.nota2)
+    };
+    getNota3 (){
+        return  cy.get(this.nota3)
+    };
+    getNota4 (){
+        return  cy.get(this.nota4)
+    };
 }
--- Testes Web/cypress/support/steps/listagemDeFilmes.step.js
+++ Testes Web/cypress/support/steps/listagemDeFilmes.step.js
@@ -18,24 +18,41 @@ const pageLogin = new LoginPage
 
 Given("que entrei na pagina inical", () => {
     cy.visit('');
+});
 
+Before({tags:"@listaDestaque"}, ()=>{
+    cy.intercept('GET', 'https://raromdb-3c39614e42d4.herokuapp.com/api/movies', {
+        fixture: "filmes.json",
+    }).as("listaFilmes");
+});
+
+Before({tags:"@listaBemAvaliados"}, ()=>{
+    cy.intercept('GET', 'https://raromdb-3c39614e42d4.herokuapp.com/api/movies?sort=true', {
+        fixture: "filmes.json",
+    }).as("lista_Filmes")
+});
+
+Before({tags:"@listaBemAvaliados1"}, ()=>{
+    cy.intercept('GET', 'https://raromdb-3c39614e42d4.herokuapp.com/api/movies?sort=true', {
+        fixture: "filme.json",
+    }).as("lista_Filmes")
+});
+
+Before({tags:"@listaDestaque1"}, ()=>{
+    cy.intercept('GET', 'https://raromdb-3c39614e42d4.herokuapp.com/api/movies', {
+        fixture: "filme.json",
+    }).as("listaFilmes");
 });
 
 Then('é possivel ver a lista de filmes cadastrados', () => {
-    pageLista.getRegistro();
-    pageLista.getLogin();
-    pageLista.getLogo();
-    pageLista.getPesquisa();
-    pageLista.getEnviar();
-    pageLista.getFilmes();
-    pageLista.getDestaque();
-    pageLista.getFilmesEmDestaque();
-    pageLista.getDdireita();
-    pageLista.getDesquerda();
-    pageLista.getBemAvaliados();
-    pageLista.getFilmesBemAvaliados();
-    pageLista.getBAesquerda();
-    pageLista.getBAdireita();
+    pageLista.getDestaque().should('be.visible');
+    pageLista.getFilmesEmDestaque().should('be.visible');
+    pageLista.getDdireita().should('be.visible');
+    pageLista.getDesquerda().should('be.visible');
+    pageLista.getBemAvaliados().should('be.visible');
+    pageLista.getFilmesBemAvaliados().should('be.visible');
+    pageLista.getBAesquerda().should('be.visible');
+    pageLista.getBAdireita().should('be.visible');
 
 });
 
@@ -46,10 +63,10 @@ Before({ tags: "@createUser" }, () => {
     });
 });
 
-Before({tags: "@login"}, ()=>{
+Before({ tags: "@login" }, () => {
     cy.visit('');
     pageLista.getLogin();
-    pageLogin.login(email,password);
+    pageLogin.login(email, password);
     cy.wait(2000);
 })
 
@@ -63,41 +80,81 @@ After({ tags: "@deleteUser" }, () => {
     });
 });
 
-When('existem mais de 4 filmes cadastrados', () =>{
-    cy.intercept('GET','https://raromdb-3c39614e42d4.herokuapp.com/api/movies',{
-       fixture: "filmes.json",
-    }).as("listaFilmes");
-    cy.intercept('GET','https://raromdb-3c39614e42d4.herokuapp.com/api/movies?sort=true',{
-        fixture: "filmes.json",
-    }).as("lista_Filmes")
+When('existem mais de 5 filmes cadastrados', () => {
+    cy.wait('@listaFilmes')
 });
 
-Then('é possivel passar os carrosseis para os lados', ()=>{
-    pageLista.getDesquerda2();
+Then('é possivel passar os carrosseis para os lados', () => {
+    pageLista.getDesquerda2().should('be.visible').should('not.be.enabled');
+    pageLista.getDdireita().should('be.visible').should('be.enabled');
     pageLista.getDdireita1();
-    pageLista.getDdireita2();
+    pageLista.getDdireita2().should('be.visible').should('not.be.enabled');
+    pageLista.getDesquerda().should('be.visible').should('be.enabled');
     pageLista.getDesquerda1();
-    pageLista.getBAesquerda2();
+    pageLista.getBAesquerda2().should('be.visible').should('not.be.enabled');
+    pageLista.getBAdireita().should('be.visible').should('be.enabled');
     pageLista.getBAdireita1();
-    pageLista.getBAdireita2();
+    pageLista.getBAdireita2().should('be.visible').should('not.be.enabled');
+    pageLista.getBAesquerda().should('be.visible').should('be.enabled');
     pageLista.getBAesquerda1();
 });
 
-Then('a lista dos mais bem avaliados deve estar em ordem do maior para o menor', ()=>{
-
+Then('a lista dos mais bem avaliados deve estar em ordem do maior para o menor', () => {
+    pageLista.getNota1().should('contain', '100%');
+    pageLista.getNota2().should('contain', '80%');
+    pageLista.getNota3().should('contain', '60%');
+    pageLista.getNota4().should('contain', '40%');
 });
 
-Before({tags: "@loginApi"}, ()=>{
+Before({ tags: "@loginApi" }, () => {
     cy.criarFilmeAdm(email, password);
 })
 
-When('nao esxitem filmes na lista de filmes',()=>{
-    cy.intercept('GET','https://raromdb-3c39614e42d4.herokuapp.com/api/movies',{
+Before({ tags: "@listaVazia" }, () => {
+    cy.intercept('GET', 'https://raromdb-3c39614e42d4.herokuapp.com/api/movies', {
         fixture: "listaVazia.json",
-     }).as("listaVazia");
+    }).as("listaVazia");
+})
+When('nao esxitem filmes na lista de filmes', () => {
+    cy.get('@listaVazia').then((response)=>{
+        expect(response).to.be.null
+    })
 })
 
-Then('uma mensagem deve avisar ao usuario', ()=>{
+Then('uma mensagem deve avisar ao usuario', () => {
     cy.wait('@listaVazia')
-    pageLista.getMsgerro();
+    pageLista.getMsgerro().should('be.visible').should('have.text', 'Ops! Parece que ainda não temos nenhum filme.');
+})
+
+Then('todos os botoes da paginaçao devem funcionar', () => {
+    pageLista.getRegistro().should('be.visible');
+    pageLista.getLogin().should('be.visible');
+    pageLista.getLogo().should('be.visible'); 
+    pageLista.getPesquisa().should('be.visible');
+    pageLista.getEnviar().should('be.visible');
+    pageLista.getFilmes().should('be.visible');
+})
+
+When('existe algum filme ou filmes cadastrados', () => {
+    cy.wait('@listaFilmes')
+    pageLista.getFilmesEmDestaque().should('be.visible');
+    pageLista.getFilmesBemAvaliados().should('be.visible');
+});
+
+Then('é possivel ver mais detalhes dos filmes', () => {
+
+    pageLista.getCardFilme().should('be.visible');
+    pageLista.getDetalhes().should('be.visible');
+});
+
+Then('nao posso usar o carrossel', ()=>{
+    pageLista.getDesquerda2().should('be.visible').should('not.be.enabled');
+    pageLista.getBAdireita2().should('be.visible').should('not.be.enabled');
+    pageLista.getBAesquerda2().should('be.visible').should('not.be.enabled');
+    pageLista.getDdireita2().should('be.visible').should('not.be.enabled');
+});
+
+When('existe um filme cadastrado', ()=>{
+    pageLista.getFilmesEmDestaque().should('be.visible');
+    pageLista.getFilmesBemAvaliados().should('be.visible');
 })
